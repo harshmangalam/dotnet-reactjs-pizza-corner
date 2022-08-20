ded903440bfeba81e9f244f91b89f321f620988f
harshmangalam <harshdev8218@gmail.com>
2022-08-20 15:57:37 +0530

    added tooltip in profile menu

--- web-client/components/ProfileMenu.jsx
+++ web-client/components/ProfileMenu.jsx
@@ -7,14 +7,17 @@ import {
   Avatar,
   HStack,
   Text,
+  Tooltip,
 } from "@chakra-ui/react";
 import { IoLogOutOutline } from "react-icons/io5";
 export default function ProfileMenu() {
   return (
     <Menu>
-      <MenuButton aria-label="Profile Manu">
-        <Avatar name={"Harsh Mangalam"} size={"sm"} src={""} />
-      </MenuButton>
+      <Tooltip label={"Profile menu"}>
+        <MenuButton aria-label="Profile Manu">
+          <Avatar name={"Harsh Mangalam"} size={"sm"} src={""} />
+        </MenuButton>
+      </Tooltip>
 
       <MenuList>
         <MenuItem as="a">
