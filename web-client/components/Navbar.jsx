782798196551952bb0b9e822a49377d3fae85618
harshmangalam <harshdev8218@gmail.com>
2022-08-21 10:54:52 +0530

    updated ui styles

--- web-client/components/Navbar.jsx
+++ web-client/components/Navbar.jsx
@@ -1,22 +1,28 @@
-import { Heading, HStack, Icon, Image } from "@chakra-ui/react";
+import { Box, Container, Heading, HStack, Icon, Image } from "@chakra-ui/react";
 import ProfileMenu from "./ProfileMenu";
 import ThemeSwitcher from "./ThemeSwitcher";
 import PizzaBasket from "./PizzaBasket";
-import { FaPizzaSlice } from "react-icons/fa";
+import { GiFullPizza } from "react-icons/gi";
 
 export default function Navbar() {
   return (
-    <HStack h={16} justify="space-between" px={4} borderBottomWidth="1px">
-      <HStack>
-        <Icon color={"yellow.500"} as={FaPizzaSlice} fontSize="5xl" />
-        <Heading fontSize={"xl"}>Pizza Shop</Heading>
-      </HStack>
+    <Box as="nav" borderBottomWidth={"1px"}>
+      <Container maxW={"container.xl"}>
+        <HStack h={16} justify="space-between">
+          <HStack>
+            <Icon color={"yellow.500"} as={GiFullPizza} fontSize="5xl" />
+            <Heading display={["none", "block"]} fontSize={"xl"}>
+              Pizza Shop
+            </Heading>
+          </HStack>
 
-      <HStack>
-        <ProfileMenu />
-        <PizzaBasket />
-        <ThemeSwitcher />
-      </HStack>
-    </HStack>
+          <HStack>
+            <ProfileMenu />
+            <PizzaBasket />
+            <ThemeSwitcher />
+          </HStack>
+        </HStack>
+      </Container>
+    </Box>
   );
 }
