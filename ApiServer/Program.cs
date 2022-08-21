d8dd277bee23ee8a6e9abac8d5189fb4669164fe
harshmangalam <harshdev8218@gmail.com>
2022-08-21 23:10:18 +0530

    configure category

--- ApiServer/Program.cs
+++ ApiServer/Program.cs
@@ -4,21 +4,25 @@ using ApiServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// add swagger for api documentation 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 builder.Services.Configure<PizzaCornerDatabaseSettings>(builder.Configuration.GetSection("PizzaCornerDatabase"));
 builder.Services.AddSingleton<CategoryService>();
+
+
 var app = builder.Build();
 
-// health check 
-app.MapGet("/", () => "Server is up and running...");
 
-// categories endpoints
-
-// get lists of categories 
-
-app.MapGet("/categories", () =>
+// configure swagger ui in development mode 
+if (app.Environment.IsDevelopment())
 {
-    return "get categories";
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 
+app.MapControllers();
 app.Run();
