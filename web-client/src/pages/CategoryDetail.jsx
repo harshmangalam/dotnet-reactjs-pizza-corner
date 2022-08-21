562a5e21727f4a52e9256baf19d5ee42c8990f20
harshmangalam <harshdev8218@gmail.com>
2022-08-21 11:34:30 +0530

    updated pizza details

--- web-client/src/pages/CategoryDetail.jsx
+++ web-client/src/pages/CategoryDetail.jsx
@@ -3,20 +3,14 @@ import Pizza from "../../components/Pizza";
 export default function CategoryDetailPage() {
   return (
     <Box>
-      <Heading
-        textAlign={"center"}
-        fontSize={"lg"}
-        fontWeight={500}
-        textTransform="uppercase"
-        mt={4}
-      >
+      <Heading fontSize={"2xl"} textTransform="uppercase" mt={4}>
         Non Veg Pizza
       </Heading>
-      <Text>
+      <Text maxW={"md"} mt={2}>
         A delight for veggie lovers! Choose from our wide range of delicious
         vegetarian pizzas, it's softer and tastier
       </Text>
-      <SimpleGrid columns={[1, 1, 2, 3]} spacing={6} mt={6}>
+      <SimpleGrid columns={[1, 1, 2, 3]} spacing={6} mt={8}>
         {[...new Array(9)].map((pizza) => (
           <Pizza />
         ))}
