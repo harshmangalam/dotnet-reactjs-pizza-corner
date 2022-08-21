137067331620bfc69bf1647025f634c413fc9afa
harshmangalam <harshdev8218@gmail.com>
2022-08-21 12:51:19 +0530

    change spacing

--- web-client/src/pages/Pizza.jsx
+++ web-client/src/pages/Pizza.jsx
@@ -2,8 +2,8 @@ import { SimpleGrid } from "@chakra-ui/react";
 import Pizza from "../../components/Pizza";
 export default function PizzaPage() {
   return (
-    <SimpleGrid columns={[1, 1, 2, 3]} spacing={6}>
-      {[...new Array(9)].map((pizza) => (
+    <SimpleGrid columns={[1, 1, 2, 3, 4]} spacing={6}>
+      {[...new Array(8)].map((pizza) => (
         <Pizza />
       ))}
     </SimpleGrid>
