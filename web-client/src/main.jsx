f9add0a57724e7370533cded7b308f1a2a5c2a45
harshmangalam <harshdev8218@gmail.com>
2022-08-22 08:15:30 +0530

    added react query

--- web-client/src/main.jsx
+++ web-client/src/main.jsx
@@ -2,13 +2,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
