42d34d2c8eed860e3cbb63b3169f3611fd2a7291
harshmangalam <harshdev8218@gmail.com>
2022-08-22 23:21:34 +0530

    added baskets

--- web-client/src/components/PizzaBasket.jsx
+++ web-client/src/components/PizzaBasket.jsx
@@ -1,7 +1,11 @@
 import {
   Avatar,
-  AvatarBadge,
   Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
   IconButton,
   Popover,
   PopoverArrow,
@@ -10,8 +14,12 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverTrigger,
+  Stack,
+  Tag,
+  Text,
   Tooltip,
 } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import { GiShoppingCart } from "react-icons/gi";
 export default function PizzaBasket() {
   return (
@@ -27,7 +35,37 @@ export default function PizzaBasket() {
         <PopoverArrow />
         <PopoverCloseButton />
         <PopoverHeader>Pizza Basket</PopoverHeader>
-        <PopoverBody></PopoverBody>
+        <PopoverBody>
+          <Stack spacing={4} mb={4}>
+            {[...new Array(6)].map((item) => (
+              <HStack justify={"space-between"}>
+                <HStack>
+                  <Avatar src="https://www.dominos.co.in/theme2/front/images/menu-images/my-nonveg.webp" />
+                  <Stack spacing={0}>
+                    <Heading fontSize={"md"}>Non veg pizza</Heading>
+                    <Text fontSize={"sm"}>Rs 234</Text>
+                  </Stack>
+                </HStack>
+                <Tag rounded={"full"} colorScheme="purple">
+                  3 Items
+                </Tag>
+              </HStack>
+            ))}
+          </Stack>
+
+          <Divider mt={4} mb={2} />
+          <Flex justifyContent={"center"}>
+            <Button
+              as={Link}
+              to="/baskets"
+              colorScheme={"blue"}
+              variant={"ghost"}
+              rounded="full"
+            >
+              Go to Baskets
+            </Button>
+          </Flex>
+        </PopoverBody>
       </PopoverContent>
     </Popover>
   );
