4f3701acfa82ed2ce226883854b921316b772dd8
harshmangalam <harshdev8218@gmail.com>
2022-08-25 06:24:24 +0530

    added increase item

--- web-client/src/context/basket.jsx
+++ web-client/src/context/basket.jsx
@@ -7,21 +7,35 @@ export default function BasketProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   const addToBasket = (newPizza) => {
-    const isAvailable = cart.find(c=>c.item._id === newPizza._id)
-    if(isAvailable){
-      return false
+    const isAvailable = cart.find((c) => c.item._id === newPizza._id);
+    if (isAvailable) {
+      return false;
     }
     setCart((cart) => [...cart, { item: newPizza, count: 1 }]);
-    return true
+    return true;
   };
 
   const removeFromBasket = (newPizza) => {
-    const filteredCart = cart.filter(c=>c.item._id !== newPizza._id)
+    const filteredCart = cart.filter((c) => c.item._id !== newPizza._id);
     setCart(filteredCart);
   };
 
+  const increaseBasketPizzaCount = (pizzaId) => {
+    const finalCart = cart.map((c) => {
+      if (c.item._id === pizzaId) {
+        return {
+          ...c,
+          count: c.count + 1,
+        };
+      }
+    });
+    setCart(finalCart);
+  };
+
   return (
-    <BasketContext.Provider value={{ cart, addToBasket }}>
+    <BasketContext.Provider
+      value={{ cart, addToBasket, removeFromBasket, increaseBasketPizzaCount }}
+    >
       {children}
     </BasketContext.Provider>
   );
