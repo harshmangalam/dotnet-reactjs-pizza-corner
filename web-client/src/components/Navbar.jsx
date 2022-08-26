999e485a98542c929cd07d620dc6a57117b2406d
harshmangalam <harshdev8218@gmail.com>
2022-08-26 20:26:04 +0530

    updated navbar

--- web-client/src/components/Navbar.jsx
+++ web-client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
           <Logo />
 
           <HStack>
-            <ProfileMenu />
+           
             <PizzaBasket />
             <ThemeSwitcher />
           </HStack>
